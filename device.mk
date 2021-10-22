1e3acecd652c9f03e8d340b9790c21174854d428
jjpprrrr <jjpprrrr@pixelexperience.org>
2021-10-22 10:44:26 +0200

    RMX1851: turn off compressed apex
    Google introduced compressed apex on android 12 [1], and the flag is
    enabled by default in updatable_apex.mk [2]. RMX1851 cannot boot
    compressed apex with encryption, so disable it.
    
    ref:
    [1] https://source.android.com/devices/tech/ota/apex#compressed-apex
    [2] https://android.googlesource.com/platform/build/+/4a1240de8a2ee5795a30df123058677d3999145e
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I7c925cba7dbd0931d8e45f98defb81b61707c3ce

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
 # setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
