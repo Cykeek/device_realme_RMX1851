d35e9d1437f3894665e6a7f4ba2e54bc618d8d02
karthick111 <karthickrko61@gmail.com>
2022-05-16 21:06:40 +0530

    RMX1851: Drop sound trigger impl
    * Fixes audio related issues when Google assistant is set as default
    
    Change-Id: I947ac7101ab04f69cf0ea16c2c3db3bd33c94c6f

--- device.mk
+++ device.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
