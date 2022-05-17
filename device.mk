04916a14d6dd40ae0a3beeac855f1eacf58ae60d
karthick111 <karthickrko61@gmail.com>
2022-05-17 19:07:46 +0530

    RMX1851: Provide NN & Power on vendor
    Change-Id: Iad9bc811ea7d43c1238e34567e4c70ed06a5720c

--- device.mk
+++ device.mk
@@ -320,7 +320,7 @@ PRODUCT_PACKAGES += \
 
 # Neuralnetworks
 PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.2.vendor
+    android.hardware.neuralnetworks@1.3.vendor
 
 # Parts
 $(call inherit-product, packages/apps/RealmeParts/parts.mk)
@@ -330,6 +330,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
