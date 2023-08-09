b3cc7ff117e9103e9f424245168093b810d7eddd
Cykeek <Cykeek@proton.me>
2023-08-09 15:35:00 +0530

    RMX1851: uprev android.hardware.secure_element hidl Fix: I hwservicemanager: getTransport: Cannot find entry android.hardware.secure_element@1.2::ISecureElement/SIM1 in either framework or device manifest E HidlServiceManagement: Service android.hardware.secure_element@1.2::ISecureElement/SIM1 must be in VINTF manifest in order to register/get
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -323,6 +323,11 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.1 \
+    android.hardware.secure_element@1.1.vendor \
+    android.hardware.secure_element@1.2 \
+    android.hardware.secure_element@1.2.vendor \
     libjson \
     librmnetctl
 
