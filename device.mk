71dc61e623098da3dad5bf15fbf4d9b3ff460fb6
pjgowtham <pjgowtham@gmail.com>
2023-08-12 13:19:05 +0530

    RMX1851: Use qti vibrator aidl hal
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -440,8 +440,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service \
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:vendor/etc/excluded-input-devices.xml
 
 # VNDK
 VNDK_SP_LIBRARIES := \
